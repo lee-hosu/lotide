23da718cfd67afd0fa929c75277818287c814acd
Hosu <sogeumhosu@gmail.com>
2023-06-18 16:03:26 -0700

    Created assertArraysEqual function to console.log the result of eqArrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed 🥳`);
+  } else {
+    console.log(`❌ Assertion Failed 🥲`);
+  }
+};
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
