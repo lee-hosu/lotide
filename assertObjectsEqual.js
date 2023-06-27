370a446adec6daa3c6c976bc5f2195a980298878
Hosu Lee <sogeumhosu@gmail.com>
2023-06-27 03:37:53 +0000

    Implemented assertObjectsEqual function for comparing and logging equality of two objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const eqObjects = function (object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+};
+
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(
+      `✅ Assertion Passed 🥳: ${inspect(actual)} === ${inspect(expected)}`
+    );
+  } else {
+    console.log(
+      `❌ Assertion Failed 🥲 ${inspect(actual)} !== ${inspect(expected)}`
+    );
+  }
+};
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+};
+
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
+};
+
+assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject);
+assertObjectsEqual(multiColorShirtObject, longSleeveMultiColorShirtObject);
