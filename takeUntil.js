5cbe0a8550f337d9e910a023de5eed7a9a8572df
Hosu Lee <sogeumhosu@gmail.com>
2023-06-27 06:04:47 +0000

    Implemented takeUntil function to extract elements from an array until specified condition is met

---
+++ takeUntil.js
@@ -0,0 +1,44 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed 🥳 ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed 🥲 ${arr1} !== ${arr2}`);
+  }
+};
+
+const takeUntil = function (array, callback) {
+  let result = array.map(callback);
+  return array.slice(0, result.indexOf(true));
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
