33104a6e881810e09dc0c90727d3ddec1e8c5321
Hosu <sogeumhosu@gmail.com>
2023-07-06 08:34:19 -0700

    Refactor test code from head.js and assertEquals.js

--- head.js
+++ head.js
@@ -1,17 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function (arr) {
   return arr[0];
 };
-
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
