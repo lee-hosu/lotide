17250ef5254bcfbcb59b52a3c678bd92c974cb7a
Hosu <sogeumhosu@gmail.com>
2023-07-06 18:15:51 -0700

    Refactor test code

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
@@ -15,8 +8,4 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
